24a5dde7a7ad78fc2a73397980defab309f829d8
Stefan Kühnel <github-contributors@stefankuehnel.com>
2020-09-05 13:17:37 +0200

    Fix Avatar Images without Dimensions Bug

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,20 +2,22 @@ import styles from "./Header.module.css";
 import Link from "next/link";
 
 export function Header() {
-	return (
-		<header className={styles.header}>
-			<div className={styles.info}>
-				<h1 className={styles.name}>{" Linkdienst "}</h1>
-			</div>
-			<Link href="/">
-				<a>
-					<img
-						className={styles.avatar}
-						alt="Stefan Kühnel"
-						src="https://assets-cdn.stefankuehnel.com/images/modules/site/favicon/mask-icon.svg"
-					></img>
-				</a>
-			</Link>
-		</header>
-	);
+  return (
+    <header className={styles.header}>
+      <div className={styles.info}>
+        <h1 className={styles.name}>{" Linkdienst "}</h1>
+      </div>
+      <Link href="/">
+        <a>
+          <img
+            className={styles.avatar}
+            alt="Stefan Kühnel"
+            width="50"
+            height="50"
+            src="https://assets-cdn.stefankuehnel.com/images/modules/site/favicon/mask-icon.svg"
+          ></img>
+        </a>
+      </Link>
+    </header>
+  );
 }
