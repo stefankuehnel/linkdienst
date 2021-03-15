9f3fdc5890ed055a292b7514846df32089f9111b
Stefan Kühnel <git@stefankuehnel.com>
2021-03-15 01:32:43 +0100

    Use Single Quotes Instead of Double Quotes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,45 +2,41 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
-  plugins: ["simple-import-sort"],
+  plugins: ['simple-import-sort'],
   settings: {
     react: {
-      version: "detect",
-    },
+      version: 'detect'
+    }
   },
   env: {
     browser: true,
     amd: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended", // Make sure this is always the last element in the array.
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended' // Make sure this is always the last element in the array.
   ],
   rules: {
-    "prettier/prettier": [
-      "error",
-      { endOfLine: "auto" },
-      { usePrettierrc: true },
-    ],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
+    'prettier/prettier': ['error', { endOfLine: 'auto' }, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
       {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
-      },
-    ],
-  },
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton']
+      }
+    ]
+  }
 };
